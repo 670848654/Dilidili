4bbd837d49c075746e104322daee201a78f27762
670848654 <lz670848654@gmail.com>
2019-01-02 11:42:04 +0800

    add

---
+++ app/src/main/java/anime/project/dilidili/adapter/FavoriteListAdapter.java
@@ -0,0 +1,24 @@
+package anime.project.dilidili.adapter;
+
+import android.content.Context;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+import anime.project.dilidili.R;
+import anime.project.dilidili.bean.AnimeListBean;
+import anime.project.dilidili.util.Utils;
+
+public class FavoriteListAdapter extends BaseQuickAdapter<AnimeListBean, BaseViewHolder> {
+    public FavoriteListAdapter(List list) {
+        super(R.layout.item_favorite, list);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, AnimeListBean item) {
+        Utils.setImageVertical(item.getImg(),helper.getView(R.id.img));
+        helper.setText(R.id.title, item.getTitle());
+    }
+}
